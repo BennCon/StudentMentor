b52c90ccc09d491d30d0f02621f479d17ee06bb9
Benjamin Consterdine <bconsterdine1@sheffield.ac.uk>
2021-03-03 22:17:22 +0000

    Added all user types to database schema

--- db/mentee_schema.sql
+++ db/mentee_schema.sql
@@ -4,6 +4,26 @@ CREATE TABLE mentee (
     surname TEXT,
     course TEXT,
     year_of_study INTEGER,
+    has_mentor INTEGER,
+    email TEXT,
+    password TEXT
+);
+
+CREATE TABLE mentor (
+    id INTEGER PRIMARY KEY,
+    first_name TEXT,
+    surname TEXT,
+    company TEXT,
+    number_of_mentees INTEGER,
+    seeking_mentee INTEGER,
+    email TEXT,
+    password TEXT
+);
+
+CREATE TABLE admin (
+    id INTEGER PRIMARY KEY,
+    first_name TEXT,
+    surname TEXT,
     email TEXT,
     password TEXT
 );
