cb0e9483ad278e16f27efc12b27f237ccbfad3d8
Benjamin Consterdine <bconsterdine1@sheffield.ac.uk>
2021-03-05 16:33:05 +0000

    Added requires to app.rb for helpers, database, models

--- app.rb
+++ app.rb
@@ -3,4 +3,15 @@ require "sinatra"
 
 set :bind, '0.0.0.0'
 
+get "/" do
+    erb :mentee_form
+end
+    
+# Database
+require_relative "db/db"
+
+# App
+require_all "models"
 require_all "controllers"
+require_all "helpers"
+
