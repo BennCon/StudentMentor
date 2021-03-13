7943aee90259580fd80ecfd43cda9067592ae4a6
Benjamin Consterdine <bconsterdine1@sheffield.ac.uk>
2021-03-13 18:09:13 +0000

    Basic login functionality now works

--- app.rb
+++ app.rb
@@ -6,6 +6,10 @@ set :bind, '0.0.0.0'
 # So we can escape HTML special characters in the view
 include ERB::Util
 
+# Sessions
+enable :sessions
+set :session_secret, "$g]Rd2M/WbJ`~~<GZWdH@Fm'ESk2_gckCtLJJkySYG"
+
 # Database
 require_relative "db/db"
 
--- controllers/index.rb
+++ controllers/index.rb
@@ -1,3 +1,25 @@
 get "/index" do
+  @user = User.new
   erb :index
 end
+
+post '/index' do
+  @user = User.new
+  @user.load(params)
+  @error = nil
+
+  if @user.exist?
+    session[:logged_in] = true
+    redirect "/my-account"
+  else
+    @error = "Username/Password combination incorrect"
+#     redirect "/index"
+  end
+
+  erb :index
+end
+
+# get "/logout" do
+#   session.clear
+#   erb :logout
+# end
