5a1f333f2673e71c21bafbf529185807330c1099
Harry Bolton <hjbolton1@sheffield.ac.uk>
2021-03-15 16:10:04 +0000

    Added methods for registration testing and created incomplete tests

--- helpers/spec_helper.rb
+++ helpers/spec_helper.rb
@@ -27,5 +27,28 @@ SimpleCov.coverage_dir "coverage"
 
 def register_mentee
     visit "/index"
-    
+    click_button "mentee-signup"
+    fill_in "first_name", with: "First"
+    fill_in "surname", with: "Surname"
+    fill_in "course", with: "COM1001"
+    select 'Male', from: "gender"
+    select '1st', from: "year_of_study"
+    fill_in "email", with: "test@test.com"
+    fill_in "password", with "Testpass"
+    click_button "mentee_submit"
+end
+
+def register_mentor
+    visit "/index"
+    click_button "mentor-signup"
+    fill_in "first_name", with: "First"
+    fill_in "surname", with: "Surname"
+    select 'Male', from: "gender"
+    select 'Aerospace', from: "industry"
+    fill_in "company", with: "Testcompany"
+    choose 'yes'
+    select "Accounting", from: "degree_field"
+    fill_in "email", with: "test@test.com"
+    fill_in "password", with "Testpass"
+    click_button "mentor_submit"
 end
--- spec/features/register_account.rb
+++ spec/features/register_account.rb
@@ -4,7 +4,14 @@ require_relative "../../helpers/spec_helpers.rb"
 RSpec.describe do
     describe "Registration" do
         it "Creates a mentees account"
-        register_mentee
+            register_mentee
+            TODO - add a check for logged in when profile is created
+        end
+        
+        it "Creates a mentors account"
+            register_mentor
+            TODO - add a check for logged in when profile is created
+        end
     end
 end
 =end
