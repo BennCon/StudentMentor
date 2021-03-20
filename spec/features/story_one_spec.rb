1b946f1a25acf6a89a91f25c59002c3780b867e8
Harry Bolton <hjbolton1@sheffield.ac.uk>
2021-03-20 13:47:15 +0000

    Fixed typo in story one test code

--- spec/features/story_one_spec.rb
+++ spec/features/story_one_spec.rb
@@ -1,12 +1,12 @@
 require_relative "../../helpers/spec_helper.rb"
 
 describe "The sign up pages" do
-    it "Create a mentee accounts" do
+    it "Can create mentee accounts" do
         register_log_in_mentee
         expect(page).to have_content "MenteeFirst"
     end
         
-    it "Create a mentor accounts" do
+    it "Can create mentor accounts" do
         register_log_in_mentor
         expect(page).to have_content "MentorFirst"
     end
