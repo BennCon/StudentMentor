67d0aa534b6d9288933044fb36ad7f2763d4c88c
Harry Bolton <hjbolton1@sheffield.ac.uk>
2021-03-20 13:39:07 +0000

    Completed story two acceptance test

---
+++ spec/features/story_two_spec.rb
@@ -0,0 +1,11 @@
+require_relative "../../helpers/spec_helper.rb"
+
+describe "The edit profile system" do
+    it "Is able to alter a mentee's details" do
+        register_log_in_mentee
+        click_button "accEdit"
+        fill_in "biography", with: "Lorem Ipsum"
+        click_button "save"
+        expect(page).to have_content "Biography: Lorem Ipsum"
+    end
+end
