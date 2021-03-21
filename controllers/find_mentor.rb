7afe82798a72809d890d96c4dc12ec4e409d22b4
Ana Camarasan <accamarasan1@sheffield.ac.uk>
2021-03-21 21:44:11 +0000

    added the filters but they don't work properly yet

--- controllers/find_mentor.rb
+++ controllers/find_mentor.rb
@@ -3,10 +3,18 @@ get "/find-mentor" do
    id = session[:id]
    @mentee = Mentee[id] if Mentee.id_exists?(id)
    @user = User[id]
-   @dataset = DB[:mentors]
-   @num_rows = @dataset.count
+   @dataset = DB[:mentors] 
 
+   @gender = params.fetch("gender", "").strip
+   @industry = params.fetch("industry", "").strip
+   @degree = params.fetch("degree_field", "").strip
+   @sheff_grad = params.fetch("sheffield_graduate", "").strip
+    
+   @mentors = if @gender.empty? && @industry.empty? && @degree.empty? && @sheff_grad.empty?
+                DB[:mentors]
+              else
+                Mentor.where(Sequel.like(:gender, "%#{@gender}%") && Sequel.like(:industry, "%#{@industry}%") && Sequel.like(:degree_field, "%#{@degree}%") && Sequel.like(:sheffield_graduate, "%#{@sheff_grad}%")) 
+              end
 
-   
   erb :find_mentor
 end
