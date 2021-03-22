18cbebbb487ec181d4b941d074b78c503ef8a606
Harry Bolton <hjbolton1@sheffield.ac.uk>
2021-03-22 13:43:56 +0000

    Completed story six acceptance tests

---
+++ spec/features/story_six_spec.rb
@@ -0,0 +1,23 @@
+require_relative "../../helpers/spec_helper.rb"
+
+describe "An admin" do
+    
+    it "can register with a correct code" do
+        Code.unrestrict_primary_key
+        code = Code.new(id: 75, code: "something123", used: 0)
+        code.save_changes
+        register_admin "something123","admin1@test.com"
+        expect(page).to have_content "sign in"
+    end
+    
+    it "cannot register with an in-use code" do
+        register_admin "something123","admin2test@test.com"
+        expect(page).not_to have_content "sign in"
+    end
+    
+    it "cannot register with an incorrect code" do
+        register_admin "different","admin3test@test.com"
+        expect(page).not_to have_content "sign in"
+        #clear_db
+    end
+end
