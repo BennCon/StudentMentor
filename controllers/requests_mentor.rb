5288858aa8fa2055c62a54637182841a39f9ee71
Ana Camarasan <accamarasan1@sheffield.ac.uk>
2021-03-22 11:14:36 +0000

    fixed the routes for requests and get help

--- controllers/requests_mentor.rb
+++ controllers/requests_mentor.rb
@@ -1,6 +1,6 @@
 get "/requests-mentor" do
 #   redirect "/index" unless session[:logged_in]
-  id = params["id"]
+  id = session[:id]
   @mentor = Mentor[id] if Mentor.id_exists?(id)
   @user = User[id]
   erb :requests_mentor
