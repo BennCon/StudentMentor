2cb6c007262620a464acfebc096f5b9c0b3164f5
Charlie Grimshaw <cgrimshaw1@sheffield.ac.uk>
2021-03-22 13:18:47 +0000

    very basic README.md skeleton

--- spec/unit/get_help_mentor_spec.rb
+++ spec/unit/get_help_mentor_spec.rb
@@ -1,15 +1,14 @@
 require_relative "../../helpers/spec_helper"
 
 RSpec.describe "Get Help" do
-    describe "GET /get-help-mentee" do
-        
+    describe "GET /get-help-mentor" do
         before(:all) do
             User.unrestrict_primary_key
-            Mentee.unrestrict_primary_key
-            user = User.new(email: "testtest@test.test", password: "test1234", user_type: "mentee", id:9999)
+            Mentor.unrestrict_primary_key
+            user = User.new(email: "testtest@test.test", password: "test1234", user_type: "mentor", id:9999)
             user.save_changes
-            mentee = Mentee.new(first_name: "A", surname: "B", course: "ABCD", year_of_study: 9999, email: "testtest@test.test", password: "test1234", id:9999)
-            mentee.save_changes
+            mentor = Mentor.new(first_name: "A", surname: "B", industry: "ABCDEF", company:"ABCDE", email: "testtest@test.test", password: "test1234", sheffield_graduate: true, degree_field: "ABCD", id:9999)
+            mentor.save_changes
             post "/", "email" => "testtest@test.test", "password" => "test1234"
         end
         
@@ -21,12 +20,12 @@ RSpec.describe "Get Help" do
         end
         
         it "has a status code of 200 (OK)" do
-            get "/get-help-mentee"
+            get "/get-help-mentor"
             expect(last_response.status).to eq(200)
         end
         
         it "includes a frequently asked question section" do
-            get "/get-help-mentee"
+            get "/get-help-mentor"
             expect(last_response.body).to include('FAQ')
         end
     end
