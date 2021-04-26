2f1869c820272b032b506d3eda98d2c69c6053f0
Ana Camarasan <accamarasan1@sheffield.ac.uk>
2021-04-26 16:56:43 +0000

    commented the admin validation

--- controllers/admin_form.rb
+++ controllers/admin_form.rb
@@ -15,20 +15,19 @@ post "/admin-form" do
    @user.user_type = "admin"
    
    #Checks if admin code is valid
-   if @admin.valid?
-      if @admin.valid_code?(@admin.admin_code)
-         @user.save_changes
-         @admin.save_changes
+   #if @admin.valid?
+    #  if @admin.valid_code?(@admin.admin_code)
+     #    @user.save_changes
+     #     @admin.save_changes
 
-         id = @user.id
-         @admin.id = id
+      #   id = @user.id
+      #   @admin.id = id
 
-         @user.save_changes
-         @admin.save_changes
-         redirect "/"
-      end
-   end
-   
+       #  @user.save_changes
+        # @admin.save_changes
+        # redirect "/"
+     # end
+ #  end
    #Refreshes page if invalid
 
    erb :admin_form
