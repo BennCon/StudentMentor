aad6e9f3eeb295b84a81035625eb82fe04771b01
Charlie Grimshaw <cgrimshaw1@sheffield.ac.uk>
2021-04-27 19:17:25 +0000

    added a test to increase coverage

--- spec/unit/my_account_admin_spec.rb
+++ spec/unit/my_account_admin_spec.rb
@@ -30,4 +30,30 @@ RSpec.describe "My Account Admin" do
             expect(last_response.body).to include('First name', 'Test')
         end
     end
+    
+    describe "POST /my-account-admin" do
+        
+        before(:all) do
+            User.unrestrict_primary_key
+            Admin.unrestrict_primary_key
+            user = User.new(email: "testtest@test.test", password: "test1234", user_type: "admin", id:9999)
+            user.save_changes
+            admin = Admin.new(first_name: "Test", surname: "Test", admin_code: "1234", email: "testtest@test.test", password: "test1234", id:9999)
+            admin.save_changes
+            post "/", "email" => "testtest@test.test", "password" => "test1234"
+        end
+        
+        after(:all) do
+            DB.from("mentees").delete
+            DB.from("mentors").delete
+            DB.from("admins").delete
+            DB.from("users").delete
+        end
+        
+        it "has a status code of 200 (OK)" do
+            post "/my-account-admin"
+            expect(last_response).to be_redirect
+            expect(last_response.location).to include('/my-account-admin')
+        end
+    end
 end
