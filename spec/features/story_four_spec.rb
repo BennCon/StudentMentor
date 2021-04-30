3bd6435e624dbc616e98ea571aa6779c64831ea5
Harry Bolton <hjbolton1@sheffield.ac.uk>
2021-04-30 16:20:21 +0000

    Added extra tests to story four testing to better meet acceptance criteria

--- spec/features/story_four_spec.rb
+++ spec/features/story_four_spec.rb
@@ -2,11 +2,18 @@ require_relative "../../helpers/spec_helper.rb"
 
 describe "The Filter System" do
     
-    it "filters by gender" do
+    it "shows all available mentors when no filter is selected" do
         register_mentor "Mister","Planes","planes123","Male","Aerospace","Aerospace","planes123@test.com"
         register_mentor "Miss","Plants","plants123","Female","Agriculture","Agriculture","plants123@test.com"
         
         register_mentee "MenteeFirst","MenteeSur","TestMentee","Male","mentee123@test.com"
+        log_in "TestMentee"
+        click_link '> Find A Mentor'
+        expect(page).to have_content "Mister"
+        expect(page).to have_content "Miss"
+    end
+    
+    it "filters by gender" do
         log_in "TestMentee"
         click_link '> Find A Mentor'
         click_button "Filters"
@@ -34,6 +41,17 @@ describe "The Filter System" do
         click_button "Apply filters"
         expect(page).to have_content "Mister"
         expect(page).not_to have_content "Miss"
+    end
+    
+    it "displays a message if no mentors are available with the specified filters" do
+        log_in "TestMentee"
+        click_link '> Find A Mentor'
+        click_button "Filters"
+        select "Tourism", from: "industry"
+        click_button "Apply filters"
+        expect(page).not_to have_content "Mister"
+        expect(page).not_to have_content "Miss"
+        expect(page).to have_content "No Mentors"
         clear_db
     end
 end
