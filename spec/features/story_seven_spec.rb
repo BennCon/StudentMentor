7fba0af83c3de8cf0ab68b796c74283ff6627b65
Harry Bolton <hjbolton1@sheffield.ac.uk>
2021-04-30 16:28:22 +0000

    Minor change to story 7 tests, now checks that it is on the correct sign up page

--- spec/features/story_seven_spec.rb
+++ spec/features/story_seven_spec.rb
@@ -5,18 +5,21 @@ describe "The splash page gives the option to" do
     it "sign up as a mentee" do
         visit "/"
         click_link('sign up as a mentee')
-        expect(page).to have_content "Submit"
+        expect(page).to have_content "Signing up"
+        expect(page).to have_content "MENTEE"
     end
     
     it "sign up as a mentor" do
         visit "/"
         click_link('sign up as a mentor')
-        expect(page).to have_content "Submit"
+        expect(page).to have_content "Signing up"
+        expect(page).to have_content "MENTOR"
     end
     
     it "sign up as an admin" do
         visit "/"
         click_link('sign up as an admin')
-        expect(page).to have_content "Submit"
+        expect(page).to have_content "Signing up"
+        expect(page).to have_content "ADMIN"
     end
 end
