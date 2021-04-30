7ed8192a3f60d7c512bd50f207cdba48922dd957
Harry Bolton <hjbolton1@sheffield.ac.uk>
2021-04-30 13:05:57 +0000

    Completed story twelve acceptance testing

---
+++ spec/features/story_twelve_spec.rb
@@ -0,0 +1,17 @@
+require_relative "../../helpers/spec_helper.rb"
+
+describe "An Admin" do
+    it "Should be able to see a list of all mentees" do
+        Code.unrestrict_primary_key
+        code = Code.new(id: 65, code: "testcode", used: 0)
+        code.save_changes
+        register_admin "TestAdmin","testcode","admintest@test.com"
+        register_mentee "MenteeOne","SurOne","TestMentee1","Male","mentee123@test.com"
+        register_mentee "MenteeTwo","SurTwo","TestMentee2","Female","mentee456@test.com"
+        log_in "TestAdmin"
+        click_link '> All Mentees'
+        expect(page).to have_content("TestMentee1")
+        expect(page).to have_content("TestMentee2")
+        clear_db
+    end
+end
