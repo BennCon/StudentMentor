fcd70bad2b05ba23a3d55bad00b94302e8d24ca7
Ana Camarasan <accamarasan1@sheffield.ac.uk>
2021-05-02 19:27:39 +0000

    added the users help page and controller

---
+++ controllers/help_users.rb
@@ -0,0 +1,17 @@
+get "/help-users" do
+  redirect "/" unless session[:logged_in]
+  id = session[:id]
+  @admin = Admin[id] if Admin.id_exists?(id)
+  @user = User[id]
+  erb :help_users
+end
+
+post '/help-users' do
+   id = session[:id]
+   @faq = Faq.new
+
+   #Loads the q&a
+   faq.load(params)
+   redirect '/help-users'
+   erb :help_users
+end
