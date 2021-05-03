adf17747e114719d19a68580fbd9057083c258da
Benjamin Consterdine <bconsterdine1@sheffield.ac.uk>
2021-05-03 15:46:07 +0000

    Basic requests page for mentees working, with pending and accepted section

--- controllers/requests_mentee.rb
+++ controllers/requests_mentee.rb
@@ -3,5 +3,19 @@ get "/requests-mentee" do
   id = session[:id]
   @mentee = Mentee[id] if Mentee.id_exists?(id)
   @user = User[id]
+   
+  @requests = DB[:requests].where(mentee_id: id)
+  @mentors = DB[:mentors] 
+   
+  @mentor_req_ids = []
+  @mentor_accepted_ids = []
+  @requests.each do |request|
+     if request[:accepted] == 0
+        @mentor_req_ids << request[:mentor_id]
+     elsif request[:accepted] == 1
+        @mentor_accepted_ids << request[:mentor_id]
+     end
+  end
+    
   erb :requests_mentee
 end
