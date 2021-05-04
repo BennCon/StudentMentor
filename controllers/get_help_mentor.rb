6925bbbd2a912a64e0f8cdd52a96bd6de27ba4f2
Ana Camarasan <accamarasan1@sheffield.ac.uk>
2021-05-04 14:30:12 +0000

    now the q&a should be displayed from the database, though there may be some problem with db

--- controllers/get_help_mentor.rb
+++ controllers/get_help_mentor.rb
@@ -3,5 +3,7 @@ get "/get-help-mentor" do
   id = session[:id]
   @mentor = Mentor[id] if Mentor.id_exists?(id)
   @user = User[id]
+    
+  @faqs = Questions_answers.all
   erb :get_help_mentor
 end
