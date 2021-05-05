d525a36356959144f0d1aa7983b323c6d6384bef
Harry Bolton <hjbolton1@sheffield.ac.uk>
2021-05-05 15:34:35 +0000

    Added framework for story 5 acc tests

---
+++ spec/features/story_five_spec.rb
@@ -0,0 +1,24 @@
+require_relative "../../helpers/spec_helper.rb"
+
+describe "A user" do
+    it "can have their password changed by an admin" do
+        register_mentee "MenteeFirst","MenteeSur","TestMentee","Male","mentee123@test.com"
+        Code.unrestrict_primary_key
+        code = Code.new(id: 45, code: "qwerty", used: 0)
+        code.save_changes
+        register_admin "TestAdmin","qwerty","admintest@test.com"
+        log_in "TestAdmin"
+        click_link "> All Mentees"
+        expect(page).to have_button('change')
+        #Change the mentees password to 'newtestpass' for the next test
+    end
+    it "can only log in with a changed password" do
+        log_in "TestMentee"
+        expect(page).to have_content("Incorrect")
+        fill_in "username", with: "TestMentee"
+        fill_in "password", with: "newtestpass"
+        click_button "signin"
+        expect(page).to have_content("MenteeFirst")
+        clear_db
+    end
+end
