365e5459cb7dc837c13283c7fa05468c073509a1
Harry Bolton <hjbolton1@sheffield.ac.uk>
2021-05-07 12:48:58 +0000

    Minor changes to story four and sixteen acc tests

--- spec/features/story_sixteen_spec.rb
+++ spec/features/story_sixteen_spec.rb
@@ -1,7 +1,7 @@
 require_relative "../../helpers/spec_helper.rb"
 
 describe "An admin email" do
-    it "is available on the help page" do
+    it "is available on the help page so that users can change their password" do
         register_mentee "MenteeFirst","MenteeSur","TestMentee","Male","mentee123@test.com"
         log_in "TestMentee"
         click_link '> Get Help'
