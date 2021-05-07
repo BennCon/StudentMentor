030435af787afb0218963a461dcb5c6afbf5bf0c
Benjamin Consterdine <bconsterdine1@sheffield.ac.uk>
2021-05-07 14:49:15 +0000

    fixed issue with get help mentor page

--- controllers/get_help_mentor.rb
+++ controllers/get_help_mentor.rb
@@ -1,21 +1,10 @@
-get "/help-users" do
-  redirect "/" unless session[:logged_in]
+get "/get-help-mentor" do
+  redirect "/index" unless session[:logged_in]
   id = session[:id]
-  @admin = Admin[id] if Admin.id_exists?(id)
+  @mentee = Mentor[id] if Mentor.id_exists?(id)
   @user = User[id]
-    
+  
   @faqs = Questions_answers.all
     
-  erb :help_users
-end
-
-post "/help-users" do
-   id = session[:id]
-   faq = Questions_answers.new
-
-   #Loads the q&a
-   faq.load(params)
-   faq.save_changes
-   redirect "/help-users"
-   erb :help_users
+  erb :get_help_mentee
 end
