16167af257d46def0c3b4af061b8f7110dd352db
Harry Bolton <hjbolton1@sheffield.ac.uk>
2021-05-08 12:59:06 +0000

    Altered story sixteen acc tests to use new admin email system

--- spec/features/story_sixteen_spec.rb
+++ spec/features/story_sixteen_spec.rb
@@ -1,11 +1,21 @@
 require_relative "../../helpers/spec_helper.rb"
 
-describe "An admin email" do
-    it "is available on the help page so that users can change their password" do
+describe "Admin emails" do
+    it "are displayed to mentees" do
+        Code.unrestrict_primary_key
+        code = Code.new(id: 25, code: "acode", used: 0)
+        code.save_changes
+        register_admin "TestAdmin","acode","admintest123@test.com"
         register_mentee "MenteeFirst","MenteeSur","TestMentee","Male","mentee123@test.com"
         log_in "TestMentee"
         click_link '> Get Help'
-        expect(page).to have_content("email")
+        expect(page).to have_content("admintest123@test.com")
+    end
+    it "are displayed to mentors" do
+        register_mentor "MentorFirst","Surname","TestMentor","Male","Aerospace","Aerospace","mentortest@test.com"
+        log_in "TestMentor"
+        click_link '> Get Help'
+        expect(page).to have_content("admintest123@test.com")
         clear_db
     end
 end
