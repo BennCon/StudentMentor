cf9000e3ad975c17ade5e339d1c73a53bac844ba
Harry Bolton <hjbolton1@sheffield.ac.uk>
2021-05-09 19:48:55 +0000

    Minor changes to story twenty one

--- spec/features/story_twentyone_spec.rb
+++ spec/features/story_twentyone_spec.rb
@@ -12,9 +12,14 @@ describe "Help Page updates" do
         expect(page).to have_button('add_faq')
         # Need to add FAQs to the help page so they can be checked
         click_button 'add_faq'
-        fill_in "question", with: "TestQuestion"
-        fill_in "answer", with: "TestAnswer"
+        fill_in "question", with: "EETestQuestion"
+        fill_in "answer", with: "EETestAnswer"
         choose 'mentee'
+        click_button "save"
+        click_button 'add_faq'
+        fill_in "question", with: "ORTestQuestion"
+        fill_in "answer", with: "ORTestAnswer"
+        choose 'mentor'
         click_button "save"
     end
     it "can be seen by mentees" do
@@ -22,8 +27,17 @@ describe "Help Page updates" do
         log_in "TestMentee"
         click_link '> Get Help'
         # The added FAQ should be visible to the mentee
-        expect(page).to have_content("TestQuestion")
-        expect(page).to have_content("TestAnswer")
+        expect(page).to have_content("EETestQuestion")
+        expect(page).to have_content("EETestAnswer")
+        click_link '> Logout'
+    end
+    it "can be seen by mentors" do
+        register_mentor "MentorFirst","Surname","TestMentor","Male","Aerospace","Aerospace","mentortest@test.com"
+        log_in "TestMentor"
+        click_link '> Get Help'
+        # The added FAQ should be visible to the mentor
+        expect(page).to have_content("ORTestQuestion")
+        expect(page).to have_content("ORTestAnswer")
         clear_db
     end
 end
